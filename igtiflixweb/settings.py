0d4c08af62a4fe47d295014b531b24b8073ae6c2
Mlluiz39 <mlluizpereira39@gmail.com>
2020-10-24 17:29:49 -0300

    commit showsenv

--- igtiflixweb/settings.py
+++ igtiflixweb/settings.py
@@ -40,7 +40,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'inicio',
-    'genero'
+    'genero',
+    'serie'
 ]
 
 MIDDLEWARE = [
--- igtiflixweb/urls.py
+++ igtiflixweb/urls.py
@@ -6,6 +6,7 @@ from inicio import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path(route='inicio/', view=include('inicio.urls')),
-    path(route='genero/', view=include('genero.urls'))
+    path(route='inicio/',view=include('inicio.urls')),
+    path(route=r'genero/',view=include('genero.urls')),
+path(route=r'serie/', view=include('serie.urls'))
 ]
